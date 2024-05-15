3a69ac57a3e276690aeb822672b4d5e0ff6edc3c
gabrielfalanga <gabrielmartinsfalanga@gmail.com>
2024-05-14 23:48:20 -0300

    titulo e clear adicionados

--- main.py
+++ main.py
@@ -1,32 +1,49 @@
 from random import randint
+import os
+
+def limpar_console():
+    'Limpa o console, independente do sistema operacional.'
+    # Verifica o sistema operacional
+    if os.name == 'nt':  # Windows
+        os.system('cls')
+    else:                # Unix/Linux/MacOS
+        os.system('clear')
+
 
 t = ['ped', 'pap', 'tes']
 
-print('Olá, vamos jogar Pedra, Papel ou Tesoura!')
-nome = input('Digite o seu nome: ').title().strip()
+titulo_jogo_jokenpo = '''
+-----------------------------------
+           J O K E N P O           
+-----------------------------------
+'''
 
 pontos_jogador = 0
 pontos_pc = 0
-
 mensagem = ''
+
+print('Olá, vamos jogar Pedra, Papel ou Tesoura!')
+nome = input('Digite o seu nome: ').title().strip()
+limpar_console()
 
 def ganhou():
     global pontos_jogador
     pontos_jogador += 1
-    print('\n------------------------------------')
+    print('\n-----------------------------------')
     print(mensagem)
     print(f'Computador: {pontos_pc}\n{nome}: {pontos_jogador}')
-    print('------------------------------------')
+    print('-----------------------------------')
 
 def perdeu():
     global pontos_pc
     pontos_pc += 1
-    print('\n------------------------------------')
+    print('\n-----------------------------------')
     print(mensagem)
     print(f'Computador: {pontos_pc}\n{nome}: {pontos_jogador}')
-    print('------------------------------------')
+    print('-----------------------------------')
 
 while True:
+    print(titulo_jogo_jokenpo)
     pc = t[randint(0,2)]
     print("\nPara sair, digite 'exit'\nPara reiniciar a pontuação, digite 'reset'\n")
     jogador = input('Pedra(ped), Papel(pap) ou Tesoura(tes)? ').lower().strip()
@@ -60,7 +77,7 @@ while True:
     elif jogador == 'reset':
         pontos_jogador = 0
         pontos_pc = 0
-        print('A pontuação foi resetada!\n')
+        print('A pontuação foi resetada.\n')
         print(f'Computador: {pontos_pc}\n{nome}: {pontos_jogador}')
     elif jogador == 'exit':
         break
